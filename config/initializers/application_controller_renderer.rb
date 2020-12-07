08b186235758c1ca0422c600a59d179446dd8886
愿山聖 <asjstksmr@gmail.com>
2020-12-07 23:46:20 +0900

    [add]lessen-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '639e419a516e6666023c62c4af7c3406b326d8f0626b662d545c47b6352b3bc783b1333ff023dc034ae81a17ef2a31ec311d2ed5caa6653092f8c9d9e9f7a93a'
