77eec6ee3f47b65087d9d0ecd9b89c27196aafb7
愿山聖 <asjstksmr@gmail.com>
2020-12-08 11:44:06 +0900

    [fin]meshoterro

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -7,16 +7,28 @@ class PostImagesController < ApplicationController
 	def create
 		@post_image = PostImage.new(post_image_params)
 		@post_image.user_id = current_user.id
-		@post_image.save
-		redirect_to post_images_path
+		# インスタンス変数の理由→バリデーションチェックでfalseになった際にrenderでviewを呼び出すので
+		# viewのインスタンス変数を表示するのとエラー内容をこのアクションのインスタンス変数へ格納して表示するため
+		if @post_image.save
+			redirect_to post_images_path
+		else
+			render :new
+		end
 	end
 
 	def index
-		@post_images = PostImage.all
+		@post_images = PostImage.page(params[:page]).reverse_order
 	end
 
 	def show
 		@post_image = PostImage.find(params[:id])
+		@post_comment = PostComment.new
+	end
+
+	def destroy
+		@post_image = PostImage.find(params[:id])
+		@post_image.destroy
+		redirect_to post_images_path
 	end
 
 	private
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'post_images#index'
 
-  resources :post_images, only: [:new, :create, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]  do
+  	resources :post_comments, only: [:create, :destroy]
+  	resource :favorites, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
 end
